79a17390bd963f25e0092074d3732a89353ae153
nerv617 <nerv617@gmail.com>
2015-10-11 18:11:49 -0400

    Ready to deploy
    Many changes. Added validation. Everything works, pre-deployment.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import webapp2
 import jinja2
 import os
-import cgi
 import urllib
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -41,55 +40,36 @@ DEFAULT_WALL = 'Public'
 def wall_key(wall_name=DEFAULT_WALL):
     return ndb.Key('Wall', wall_name)
 
-class Author(ndb.Model):
-    identity = ndb.StringProperty(indexed=True)
-    name = ndb.StringProperty(indexed=False)
-    email = ndb.StringProperty(indexed=False)
 
 class Post(ndb.Model):
-    author = ndb.StructuredProperty(Author)
+    author = ndb.StringProperty(indexed=True)
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 class CommentHandler(Handler):
     def get(self):
         wall_name = self.request.get('wall_name',DEFAULT_WALL)
-        
         posts_query = Post.query(ancestor = wall_key(wall_name)).order(-Post.date)
         posts =  posts_query.fetch()
-        
-        user = users.get_current_user()
-        if user:
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-            
-        
-       
-        
-        self.render("wall.html", user=user, posts= posts, wall_name= urllib.quote_plus(wall_name), url= url, url_linktext= url_linktext)
+        self.render("wall.html", posts= posts, wall_name= urllib.quote_plus(wall_name))
 
 class PostWall(Handler):
     def post(self):
         wall_name = self.request.get('wall_name',DEFAULT_WALL)
-        post = Post(parent=wall_key(wall_name))
-        if users.get_current_user():
-            post.author = Author(
-            identity=users.get_current_user().user_id(),
-            name=users.get_current_user().nickname(),
-            email=users.get_current_user().email())
-        else:
-            post.author = Author(
-            name='anonymous@anonymous.com',
-            email='anonymous@anonymous.com')
-
-
+        post = Post(parent= wall_key(wall_name))
+        post.author = self.request.get('author')
         post.content = self.request.get('content')
-        post.put()
-        self.redirect('/comment')
+        
+        if post.content == '' or post.author == '':
+            self.redirect('/blank')
+        else:
+            post.put()
+            self.redirect('/comment')
+
+class InvalidHandler(Handler):
+    def get(self):
+        self.render("blank.html")
+        
 
 app = webapp2.WSGIApplication([('/', MainHandler), ('/notes', NoteHandler), ('/comment', CommentHandler), ('/sign', PostWall)
-, ('/new', New_notesHandler)], debug=True)
+, ('/new', New_notesHandler),('/blank', InvalidHandler)], debug=True)
